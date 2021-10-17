a69d03d434e8ba7a838086a5f43fbd44f61c1455
argas7 <argas@cin.ufpe.br>
2021-10-17 14:37:07 -0300

    Feature(association): create new controllers to handle with Address entity

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -6,3 +6,4 @@ export * from './student.controller';
 export * from './course-student.controller';
 export * from './department.controller';
 export * from './student-department.controller';
+export * from './address.controller';
