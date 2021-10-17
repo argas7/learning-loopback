fc1500c1dc7f789dff813fd8522055daff965e96
argas7 <argas@cin.ufpe.br>
2021-10-17 13:01:14 -0300

    Feature(connectiong-postgres): create Student model

---
+++ src/models/student.model.ts
@@ -0,0 +1,27 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Student extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  name?: string;
+
+
+  constructor(data?: Partial<Student>) {
+    super(data);
+  }
+}
+
+export interface StudentRelations {
+  // describe navigational properties here
+}
+
+export type StudentWithRelations = Student & StudentRelations;
