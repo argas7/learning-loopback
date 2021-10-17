d890ec3e7df7b555b22aeba72bd0893aefae87c7
argas7 <argas@cin.ufpe.br>
2021-10-17 14:36:39 -0300

    Feature(association): create a new Model, Address, to associate with Students

---
+++ src/models/address.model.ts
@@ -0,0 +1,37 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Address extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  city?: string;
+
+  @property({
+    type: 'string',
+  })
+  zip?: string;
+
+  @property({
+    type: 'string',
+  })
+  phone?: string;
+
+
+  constructor(data?: Partial<Address>) {
+    super(data);
+  }
+}
+
+export interface AddressRelations {
+  // describe navigational properties here
+}
+
+export type AddressWithRelations = Address & AddressRelations;
