43b44c3e854d9ceb1b94a90012088b4441162762
argas7 <argas@cin.ufpe.br>
2021-10-17 14:30:02 -0300

    Feature(association): create association between Student and Department (belongsTo)

--- src/repositories/student.repository.ts
+++ src/repositories/student.repository.ts
@@ -1,16 +1,22 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
 import {PostgreSqlDataSource} from '../datasources';
-import {Student, StudentRelations} from '../models';
+import {Student, StudentRelations, Department} from '../models';
+import {DepartmentRepository} from './department.repository';
 
 export class StudentRepository extends DefaultCrudRepository<
   Student,
   typeof Student.prototype.id,
   StudentRelations
 > {
+
+  public readonly department: BelongsToAccessor<Department, typeof Student.prototype.id>;
+
   constructor(
-    @inject('datasources.PostgreSQL') dataSource: PostgreSqlDataSource,
+    @inject('datasources.PostgreSQL') dataSource: PostgreSqlDataSource, @repository.getter('DepartmentRepository') protected departmentRepositoryGetter: Getter<DepartmentRepository>,
   ) {
     super(Student, dataSource);
+    this.department = this.createBelongsToAccessorFor('department', departmentRepositoryGetter,);
+    this.registerInclusionResolver('department', this.department.inclusionResolver);
   }
 }
