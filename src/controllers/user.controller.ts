c46503587c9fd8b4bdaca9037730f75a63ae36c3
argas7 <argas@cin.ufpe.br>
2021-10-18 11:41:33 -0300

    Feature(authentication): create hasher function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,20 @@
 // Uncomment these imports to begin using these cool features!
 
+import {inject} from '@loopback/core';
 import {repository} from '@loopback/repository';
 import {getJsonSchemaRef, post, requestBody} from '@loopback/rest';
 import {User} from '../models/user.model';
 import {UserRepository} from '../repositories/user.repository';
+import {BcryptHasher} from '../services/hash.password.bcrypt';
 import {validateCredentials} from '../services/user-credentials.validator';
-
-
-// import {inject} from '@loopback/core';
 
 
 export class UserController {
   constructor(
     @repository(UserRepository)
-    public userRepository: UserRepository
+    public userRepository: UserRepository,
+    @inject('service.hasher')
+    public hasher: BcryptHasher,
   ) { }
 
   @post('/signup', {
@@ -29,7 +30,12 @@ export class UserController {
   async signUp(@requestBody() userData: User) {
     validateCredentials({email: userData.email, password: userData.password});
 
-    const savedUser = await this.userRepository.create(userData);
+    const hashedPassword = await this.hasher.hashPassword(userData.password);
+
+    const savedUser = await this.userRepository.create({
+      ...userData,
+      password: hashedPassword,
+    });
 
     const returnedUser: User | {password?: string;} = savedUser;
     delete returnedUser.password;
--- src/services/user-credentials.validator.ts
+++ src/services/user-credentials.validator.ts
@@ -3,7 +3,7 @@ import validator from 'validator';
 import {Credentials} from '../repositories/user.repository';
 
 export function validateCredentials(credentials: Credentials) {
-  if (validator.isEmail(credentials.email)) {
+  if (!validator.isEmail(credentials.email)) {
     throw new HttpErrors.UnprocessableEntity('Invalid email!');
   }
 
