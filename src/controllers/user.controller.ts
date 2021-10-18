5a5f9d708e907c1854c9674d002a9c5cfb982f86
argas7 <argas@cin.ufpe.br>
2021-10-18 10:59:49 -0300

    Feature(authentication): add validations

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,6 +4,8 @@ import {repository} from '@loopback/repository';
 import {getJsonSchemaRef, post, requestBody} from '@loopback/rest';
 import {User} from '../models/user.model';
 import {UserRepository} from '../repositories/user.repository';
+import {validateCredentials} from '../services/user-credentials.validator';
+
 
 // import {inject} from '@loopback/core';
 
@@ -25,6 +27,8 @@ export class UserController {
     },
   })
   async signUp(@requestBody() userData: User) {
+    validateCredentials({email: userData.email, password: userData.password});
+
     const savedUser = await this.userRepository.create(userData);
 
     const returnedUser: User | {password?: string;} = savedUser;
