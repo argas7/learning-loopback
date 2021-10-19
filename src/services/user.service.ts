03e412ab25683b26e016392717cc8de5c98b8f00
argas7 <argas@cin.ufpe.br>
2021-10-19 12:55:56 -0300

    Feature(authentication): create user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,48 @@
+import {UserService} from '@loopback/authentication';
+import {inject} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {HttpErrors} from '@loopback/rest';
+import {securityId, UserProfile} from '@loopback/security';
+import {User} from '../models';
+import {Credentials, UserRepository} from '../repositories';
+import {BcryptHasher} from './hash.password.bcrypt';
+
+export class MyUserService implements UserService<User, Credentials> {
+  constructor(
+    @repository(UserRepository)
+    public userRepository: UserRepository,
+    @inject('service.hasher')
+    public hasher: BcryptHasher,
+  ) { }
+
+  async verifyCredentials(credentials: Credentials): Promise<User> {
+    try {
+      const foundUser = await this.userRepository.findOne({
+        where: {
+          email: credentials.email,
+        }
+      });
+
+      if (!foundUser) {
+        throw new HttpErrors.NotFound(`Error: user with email ${credentials.email} not found`);
+      }
+
+      const passwordMatch = await this.hasher.comparePassword(
+        credentials.password,
+        foundUser.password,
+      );
+
+      if (!passwordMatch) {
+        throw new HttpErrors.Unauthorized('Wrong password');
+      }
+
+      return foundUser;
+    } catch (error) {
+      throw new HttpErrors.Unauthorized(`Error: ${error}`);
+    }
+  }
+  convertToUserProfile(user: User): UserProfile {
+    return {email: user.email, name: user.name, [securityId]: `${user.id}`};
+  }
+
+}
