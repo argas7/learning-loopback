9ac1dbbc2924138f1f41cfc5bd3ea708c0232b92
argas7 <argas@cin.ufpe.br>
2021-10-20 00:17:52 -0300

    Feature(using-authoriation): create sequnce to add authentication middleware

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,46 @@
-import {MiddlewareSequence} from '@loopback/rest';
+import {
+  AuthenticateFn,
+  AuthenticationBindings,
+  AUTHENTICATION_STRATEGY_NOT_FOUND,
+  USER_PROFILE_NOT_FOUND
+} from '@loopback/authentication';
+import {inject} from '@loopback/core';
+import {FindRoute, InvokeMethod, ParseParams, Reject, RequestContext, Send, SequenceActions, SequenceHandler} from '@loopback/rest';
 
-export class MySequence extends MiddlewareSequence {}
+export class MySequence implements SequenceHandler {
+  constructor(
+    @inject(SequenceActions.FIND_ROUTE)
+    protected findRoute: FindRoute,
+    @inject(SequenceActions.PARSE_PARAMS)
+    protected parseParams: ParseParams,
+    @inject(SequenceActions.INVOKE_METHOD)
+    protected invoke: InvokeMethod,
+    @inject(SequenceActions.SEND)
+    protected send: Send,
+    @inject(SequenceActions.REJECT)
+    protected reject: Reject,
+    @inject(AuthenticationBindings.AUTH_ACTION)
+    protected authenticateRequest: AuthenticateFn,
+  ) { }
+
+  async handle(context: RequestContext) {
+    try {
+      const {request, response} = context;
+      const route = this.findRoute(request);
+
+      await this.authenticateRequest(request);
+      const args = await this.parseParams(request, route);
+      const result = await this.invoke(route, args);
+
+      this.send(response, result);
+    } catch (error) {
+      if (
+        error.code === AUTHENTICATION_STRATEGY_NOT_FOUND ||
+        error.code === USER_PROFILE_NOT_FOUND
+      ) {
+        Object.assign(error, {statusCode: 401});
+      }
+      this.reject(context, error);
+    }
+  }
+}
