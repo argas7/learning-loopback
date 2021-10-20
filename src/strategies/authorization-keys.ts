4217327fe452ec1924991757405e15979319d662
argas7 <argas@cin.ufpe.br>
2021-10-20 00:49:05 -0300

    Feature(authorization): add permissions property to user model and pass permissions on controllers

---
+++ src/strategies/authorization-keys.ts
@@ -0,0 +1,9 @@
+export const enum PermissionKeys {
+  //adim
+  CreateJob = 'CreateJob',
+  UpdateJob = 'UpdateJob',
+  DeleteJob = 'DeleteJob',
+
+  //normal authenticated user
+  AccessAuthFeature = 'AccessAuthFeature',
+}
